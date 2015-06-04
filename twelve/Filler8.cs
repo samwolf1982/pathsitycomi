ac1bd9afd5589e59fcfc2939d6fa289a8d5c464f
Sam <biryukwolf@yahoo.com>
2015-06-04 20:25:32 +0300

    нерабочая графика но прога рабочая

---
+++ twelve/Filler8.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+
+namespace twelve
+{
+    class Filler8
+    {
+        #region Values
+                //List<Vector> mainList = new List<Vector>();
+    public   List<Vector> mainList2 = new List<Vector>(); // колекция векторов
+      public List<Vector> temp = new List<Vector>();
+    //  int[] testlist = new int[] { 6, 6, 9,  0, 0 };
+      int index = 1;
+       int rank;
+      int countAlif = 0;
+     public  List<List<Vector>> mainLL = new List<List<Vector>>();
+     public Vector[,] dinamicmainLL;
+     //Переменная "локер", которая служит для блокировки value
+     private object valueLocker = new object();
+
+      int[] indexerToLevel;
+        #endregion
+
+
+     #region Debug
+    public List<int[]> testint = new List<int[]>();
+     #endregion
+
+
+    public void startThread()
+    {
+        lock (valueLocker)
+        {
+            //Выводим значение value
+            var res = alif4(0);
+            //Увеличиваем его на единицу
+                 }
+    }
+      public void start()
+      {
+          temp.Clear();
+          #region Hide
+           //temp.Add(mainList2[6]);
+          //temp.Add(mainList2[6]);
+          //temp.Add(mainList2[9]);
+          //temp.Add(mainList2[0]);
+          //temp.Add(mainList2[0]);
+          /////////*************
+          //temp.Add(mainList2[3]);
+          //temp.Add(mainList2[6]);
+          //temp.Add(mainList2[9]);
+          //Vector resTemp = new Vector();
+          //foreach (var item in temp)
+          //{
+          //    resTemp +=item;
+          //}
+          //for (int i = 0; i < mainList2.Count; i++)    // для каждого елемента из векторов свой запуск
+          //{
+          //List<Vector>vv=new List<Vector>();
+          //vv.Add(temp[0]);
+          #endregion
+         
+          var po = Math.Pow(12, 4);
+          var maxval = long.MaxValue;
+
+          for ( long i = 0; i < po; i++)
+          {
+
+              if (i == 500)
+              {
+
+              }
+              //Thread tread = new Thread(alif4);
+
+
+              var res = alif4(0);
+          }
+                    //var res=alif3(1,temp[0],vv);   // для каждого елемента из векторов свой запуск
+        // для каждого елемента из векторов свой запуск
+
+                    int result = 9999;
+  
+      
+      }
+
+      public  Vector alif4(int level)   
+      {
+         // countAlif++;
+          if (level == rank - 1)// опустился вниз и взял след вектор и вернул
+          {
+            #region Нижний уровень
+                          //return  nextVector4End(level);  // возврат самого нижнего вектор + смещение индекс      
+              return nextVector4End(level);  // возврат самого нижнего вектор + смещение индекс      
+              }
+           #endregion
+
+          var lev = level+1;
+              return  alif4(lev)+ nextVector4(level);
+      
+
+      }
+
+      #region Not Use
+            public bool alif3(int level, Vector vector, List<Vector> curentList)
+      {
+          countAlif++;
+          if (level == rank - 1)   // опустился вниз и взял след вектор и вернул
+          {
+              #region Нижний уровень
+              foreach (var item in mainList2)
+              {
+                  Vector res = item + vector;
+                  if (res.Length > 0.90 && res.Length < 1.1)
+                  {
+                      int ok = 999;
+                      // проверить на пересечение и собирать
+                      curentList.Add(item);
+                      mainLL.Add(curentList.ToList());
+                      curentList.RemoveAt(curentList.Count - 1);
+                      return true;
+                  }
+                  else
+                  {
+                      int bad = 999;
+                  }
+              }
+              return false;
+              #endregion
+              // проход по циклу последнего совпадения
+
+          }
+
+          int num = level + 1;
+          foreach (var item in mainList2)
+          {
+              var vv = item + vector;
+              var result = alif3(num, vv, curentList);
+          }
+
+          // сочетаю двв вектора
+
+          return false;
+
+      }
+
+        public List< Vector>  alif2(int level, Vector vector)
+      {
+          countAlif++;
+          if (level == rank-1)
+          {
+              #region Нижний уровень
+                       foreach (var item in mainList2)
+              {
+                  Vector res=item+vector;
+                  if (res.Length > 0.90 && res.Length < 1.1)
+                  {
+                      int ok = 999;
+                      // проверить на пересечение и собирать
+
+                  }
+                  else
+                  {
+                      int bad = 999;
+                  }
+              }
+              return new List<Vector>();
+              #endregion
+              // проход по циклу последнего совпадения
+     
+          }
+          int num = level + 1;
+          var vv = nextVector()+vector;  // сочетаю двв вектора
+          var result = alif2(num, vv);
+          return result;
+
+      }
+        private  Vector nextVector4(int level)
+        {
+            Vector v = new Vector();
+            int x = (indexerToLevel[level]);
+            v = mainList2[x];
+          //  nextValueChange(level, mainList2.Count);
+            return v;
+        }
+      #endregion
+
+        private  Vector nextVector4End(int level)
+        {
+            Vector v = new Vector();
+            int x = (indexerToLevel[level]);
+            v = mainList2[x];
+            ++indexerToLevel[level];
+            nextValueChange(level, mainList2.Count);
+            return v;
+            //Vector v = new Vector();
+            //int n = indexerToLevel[level]; // уровень - значение до 12
+            //if (n >= mainList2.Count)
+            //{   // когда n =12 тогда увеличиваеться предыдущий уровень и установка значения в 0
+            //  //  ++indexerToLevel[level - 1];
+            //    ++indexerToLevel[level-1] ;
+            //    indexerToLevel[level] = 0;
+            //    //return new Vector(); // нулевой вектор
+            //    int x1 = (indexerToLevel[level]);
+            //    v = mainList2[x1];
+
+            //    return v;
+            //}
+            //int x = (indexerToLevel[level]++);
+            //v = mainList2[x];
+            //return v;
+        }
+
+
+        public  void nextValueChange(int level,int maxVal)
+        {
+           // indexerToLevel[level]++;
+            for (int i = indexerToLevel.Length-1; i >0; i--)
+            {
+                if (indexerToLevel[i] >= maxVal) { indexerToLevel[i] = 0; indexerToLevel[i - 1]++; }
+
+            }
+
+
+        }
+        
+        private Vector nextVector()
+      {
+          Vector v=new Vector();
+          v = temp[index++];
+          return v;
+      }
+
+      public Filler8(int rank=6)
+      {
+          this.rank = rank ;
+          indexerToLevel = new int[rank];//~~~~~~~~~~~~~~~~~~~
+
+          dinamicmainLL = new Vector[rank, 4096];
+      }
+
+      #region Test Draw alif
+      public Vector alif(int level, Vector vector)
+      {
+          countAlif++;
+          if (level == rank)
+          {
+              return nextVector();
+          }
+          int num = level + 1;
+          var vv = nextVector();
+          var result = alif(num, vv) + vv;
+          return result;
+
+      }
+      public string test()
+      {
+
+          return "";
+      }
+      public List<LittleShape2> draw2()
+      {
+          List<LittleShape2> f = new List<LittleShape2>();
+          int ccc = 0;
+          foreach (var item in mainLL)
+          {
+              if (ccc++ == 1000) return f;
+              List<Point> p = new List<Point>();
+          int c = 1;
+          p.Add(new Point());
+              foreach (var i in item)
+              {
+                  Point pr = new Point(i.X * c, i.Y * c);
+                  p.Add(pr);
+                  shiftList(ref p, pr);
+                  //  shiftList(ref p, new Point(0, 0));
+                  List<Point> lp = new List<Point>();
+
+                  LittleShape2 ls = new LittleShape2();
+                  ls.setPoint(p.ToArray());
+                  f.Add(ls);
+              }
+
+
+          }
+          //foreach (var item in temp)
+          //{
+
+          //}
+
+
+
+          return f;
+      }
+      
+      public List<LittleShape2> draw()
+      {
+          List<LittleShape2> f = new List<LittleShape2>();
+          List<Point> p = new List<Point>();
+          int c = 1;
+          p.Add(new Point());
+          foreach (var item in temp)
+          {
+              Point pr = new Point(item.X * c, item.Y * c);
+              p.Add(pr);
+              shiftList(ref p, pr);
+          }
+
+          //  shiftList(ref p, new Point(0, 0));
+          List<Point> lp = new List<Point>();
+
+          LittleShape2 ls = new LittleShape2();
+          ls.setPoint(p.ToArray());
+          f.Add(ls);
+
+          return f;
+      }
+        
+      #endregion
+
+        
+        
+        #region функции из Filler
+        public void addPointToVectorMainList2()
+        {
+            // add to       mainlist2
+
+            //for (double a = 0; a <= 360; a += 30) // бежим в 11 сторон
+            for (double a = 0; a <= 360; a += 30) 
+            {
+                double xx = Math.Sin(a * Math.PI / 180);
+                double yy = Math.Cos(a * Math.PI / 180);
+                Vector v = new Vector(xx - 0, yy - 0);
+                mainList2.Add(v);
+            }
+        //    indexerToLevel = Enumerable.Range(0, mainList2.Count).ToArray();
+
+        }
+
+        public String showAllLength(List<Vector> s)
+        {
+            String str = "";
+            int x = -30;
+            int indexer = 0;
+            foreach (var item in s)
+            {
+                str += indexer++ + ") Vec: " + (x += 30).ToString() + "   (" + item.X + " : " + item.Y + ")  " + "Length: " + item.Length + "\n";
+            }
+            return str;
+        }
+
+
+        /// <summary>
+        /// смещение фигуры на ноль
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="p"></param>
+        public void shiftList(ref List<Point> arr, Point p)
+        {
+            for (int i = 0; i < arr.Count; i++)
+            {
+                var x = arr[i].X - p.X; ;// p.X;
+                var y = arr[i].Y - p.Y; ;
+                arr[i] = new Point(x, y);
+            }
+        }
+
+        #endregion
+  
+
+
+      
+    }
+}
---
+++ twelve/MainWindow.xaml.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using MoreLinq;
+using System.Threading;
+
+
+namespace twelve
+{
+      
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+     System.Windows.Threading.DispatcherTimer   dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+           
+
+     Random rand = new Random();
+        /// <summary>
+        /// нечетное количество точек пока
+        /// </summary>
+        List<PointF> arrPoints = new List<PointF>();
+        Dictionary<Line, double> arrLine = new Dictionary< Line, double>();
+
+        //случайная подстановка для цикла гамильтона
+        List<int> arrRandom = new List<int>();
+        List<int> nextarrRandom = new List<int>();
+       float t = 100;     // верхняя грань  для функции всегда декремент 
+
+        double mainValue = 0,tempNumber=0;
+        public MainWindow()
+        {
+ 
+            InitializeComponent();
+            ///start point
+            arrPoints.Add(createPoint());
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0,0,0,0,5);
+            //dispatcherTimer.Start();
+
+        }
+
+        void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (t <= 0) dispatcherTimer.Stop();
+           t = t - 0.1f;
+                String str2 = "";
+                //FlowDocument flowDoc2 = new FlowDocument(new Paragraph(new Run(str2)));
+                //   textik.Document.Blocks.Add(new Paragraph(new Run(str2)));
+                // независимый генератор
+                double d2 = randFill(ref nextarrRandom);
+                str2 = "MainValue:  " + mainValue.ToString() + "  " + "newVal:  " + d2.ToString() + " t= " + t.ToString();
+                // flowDoc2 = new FlowDocument(new Paragraph(new Run(str2 + d.ToString())));
+
+                //var sd = textik.Document.Blocks;
+                textik.Document.Blocks.Clear();
+                textik.Document.Blocks.Add(new Paragraph(new Run(str2)));
+
+                // проверка на то кто больше c предыдущим
+                if (mainValue < d2) // если путь правильный идем дальше в глубь
+                {
+                    //   mainValue = d2;
+                    //  return;
+
+                }
+                else    // а вдруг он хороший? )) вычисление критической величины и сравнивание ёё с случайной величиной
+                {
+                    // запоминаем текущее состояние 
+                    double P = 0; // критическая величина
+                    if ((mainValue - d2) > 0)
+                    {
+                        P = 100 * Math.Pow(Math.E, -(mainValue - d2) / t);
+                        // третий герератор для сравнивания результата с порогом // случайное число от некой границы в даном случае 1-100
+                        int temp = rand.Next(1, 100);
+                        if (P > temp) //  сделать анализ!!!!!!! (на ранних етапах возможность выбрать плохое решение всегда выше !!!) зависимоть от знаменателя !!
+                        {
+                            tempNumber = mainValue;
+                            mainValue = d2;
+                            System.Diagnostics.Debug.WriteLine("main=d  M=" + mainValue);
+
+                        }
+                        else
+                        {
+                            if (tempNumber != 0)
+                                mainValue = tempNumber;
+                            System.Diagnostics.Debug.WriteLine("back ");
+                        }
+
+                    }
+
+
+                }
+
+
+            
+        }
+
+        /// <summary>
+        /// случайный маршрут
+        /// </summary>
+        /// <returns></returns>
+        private double randFill(ref List<int> arr )
+        {
+            //случайная длина
+        // cлучайно заполняем массив спереди и сзади точка начала и конца (0 12468975 3 0)
+            arr = Enumerable.Range(1,arrPoints.Count-1).ToArray().OrderBy(x => rand.Next()).ToList();
+            arr.Insert(0, 0);
+            arr.Add(0);
+            // подсчет суммы
+            double sum = 0;
+            for (int i = 0;  i < arrPoints.Count-1; i++)
+            {
+                sum += lengt(arr[i], arr[i+1]);
+            }
+            // линия возврата
+            sum += lengt(arr[arr.Count-1], arr[0]);
+            return sum;
+        }
+
+
+
+ 
+        /// <summary>
+        /// линия из двух точек
+        /// </summary>
+        /// <param name="begin">первая точка</param>
+        /// <param name="end">вторая точка</param>
+        /// <returns></returns>
+        private Line createLine(int begin,int end)
+        { 
+            Line  line = new Line();
+            line.Stroke = System.Windows.Media.Brushes.Black;
+            line.X1 = arrPoints[begin].X;
+            line.X2 = arrPoints[end].X;
+            line.Y1 = arrPoints[begin].Y;
+            line.Y2 = arrPoints[end].Y;
+
+            line.StrokeThickness = 2;
+            return line;
+
+        }
+
+        private PointF createPoint()
+        {
+            return new PointF(rand.Next(10, 200), rand.Next(10, 200));
+        }
+/// <summary>
+        ///  растояние между двумя  точками
+/// </summary>
+/// <param name="begin">первая</param>
+/// <param name="end">вторая</param>
+/// <returns></returns>
+        private double lengt(int begin,int end)
+        {
+            PointF x = arrPoints[begin];
+            PointF y = arrPoints[end];
+           return Math.Sqrt( Math.Pow( (y.X-x.X),2)+Math.Pow( (y.Y-x.Y),2));
+        }
+        /// <summary>
+        /// добавляет новую точку в массив
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void but_Click(object sender, RoutedEventArgs e)
+        {
+
+            for (int i = 0; i < 50; i++)
+            {
+                 arrPoints.Add(createPoint());
+            }
+
+           
+        }
+        /// <summary>
+        /// test button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void test_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
+            for (int i = 0; i < arrPoints.Count-1; i++)
+            {
+                for (int j = i+1; j < arrPoints.Count; j++)
+                {
+                    Line l = createLine(i, j);
+                    double d= showLebgth(i, j);
+                    //все ветки графа (вес ето длина)
+                    arrLine.Add(l, d);
+                    pic.Children.Add(l);
+                      
+
+                }
+            }
+
+            // первое случайное  заполнение  // генератор для начального маршта
+            mainValue = randFill(ref arrRandom);
+            //       doubled = 99;
+
+           // String str2 = "Общая цена:  " + mainValue.ToString();
+                                            //FlowDocument flowDoc2 = new FlowDocument(new Paragraph(new Run(str2)));
+         //   textik.Document.Blocks.Add(new Paragraph(new Run(str2)));
+
+           
+        }
+        /// <summary>
+        /// отбражение длины в тексковаом поле между точками
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="str"></param>
+        /// <returns>длина между точками</returns>
+        private double showLebgth(int a,int b,string str = "")
+        {
+            double l=lengt(a, b);
+            //String str2 = "Длина от "+arrPoints[a].ToString()+" "+arrPoints[b].ToString()+":  "+l.ToString();
+            //FlowDocument flowDoc2 = new FlowDocument(new Paragraph(new Run(str2)));          
+          //  textik.Document.Blocks.Add(new Paragraph(new Run(str2)));
+            return l;
+
+        }
+
+ 
+        /// <summary>
+        /// clear
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clear_Click_1(object sender, RoutedEventArgs e)
+        {
+            textik.Document.Blocks.Clear();
+            arrPoints.Clear();
+            pic.Children.Clear();
+        }
+
+        private void tabControl1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+            if (e.Source is TabControl) //if this event fired from TabControl then enter
+            {
+                if (t1.IsSelected)
+                {
+                    //if ( FillerX == null ||    FillerX.mainList.Count == 0) return;
+                    //curentList = selectfun(1);
+                    //curentindex = 0;
+                    ////Do your job here
+                    //System.Diagnostics.Debug.WriteLine("Tab,change T1");
+                    //// инфо про фигурку
+
+                    // curentList = selectfun(1);
+                }
+                if (t10.IsSelected)
+                {
+                    //if ( FillerX == null ||    FillerX.mainList.Count == 0) return;
+                    //curentList = selectfun(1);
+                    //curentindex = 0;
+                    ////Do your job here
+                    //System.Diagnostics.Debug.WriteLine("Tab,change T1");
+                    //// инфо про фигурку
+
+                    // curentList = selectfun(1);
+                }
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// случайное заполнение
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void randFill_Click(object sender, RoutedEventArgs e)
+        {
+
+        //double d1=    randFill(ref arrRandom);
+     //       doubled = 99;
+
+            dispatcherTimer.Start();
+// MessageBox.Show("finish");
+     
+
+          
+          
+
+
+          
+            //for (int i = 0; i < sd.Count; i++)
+            //{
+            //    textik.Document.Blocks.Add(sd.);
+            //}
+          //  textik.Document.b
+
+          
+
+      
+                    
+
+        }
+
+
+
+    }
+
+}
